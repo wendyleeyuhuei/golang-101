37af10fa66d1ea47bb25d8e12d9f2ba0a902cf08
Wendy Lee <wendy.lee@foodpanda.tw>
2022-07-04 08:46:27 +0800

    Create file for goroutine demo 2

---
+++ goroutine-2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	boring("hi")
+}
+
+// Prints a message along with index infinitely,
+// and each loop sleeps for random millisecond
+func boring(msg string) {
+	for i := 0; ; i++ {
+		fmt.Println(msg, i)
+		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+	}
+}
