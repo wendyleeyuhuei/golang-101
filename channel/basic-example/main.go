6963e127ca1cc5ad27ea004da1e9da62cf7bed9d
Wendy Lee <wendy.lee@foodpanda.tw>
2022-07-04 08:34:47 +0800

    Add a method where its loop has different time interval

--- channel/basic-example/main.go
+++ channel/basic-example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -15,5 +16,14 @@ func boring(msg string) {
 	for i := 0; ; i++ {
 		fmt.Println(msg, i)
 		time.Sleep(time.Second)
+	}
+}
+
+// Prints a message along with index infinitely,
+// and each loop sleeps for random millisecond
+func slightlyLessBoring(msg string) {
+	for i := 0; ; i++ {
+		fmt.Println(msg, i)
+		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 	}
 }
